ddeabe893718b42fa43e5bdaf322d5fc71ae67ad
Adrian Molina <adrian.molina115@gmail.com>
2024-03-14 18:29:32 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+const MONGO_BBDD = process.env.MONGO_BBDD || 'mongodb://127.0.0.1:27017/gtd_project';
 const connectDB = async () =>
-	await mongoose.connect('mongodb://127.0.0.1:27017/gtd_project')
+	await mongoose.connect(MONGO_BBDD)
 		.then(res => console.log('Mongo connected'))
 		.catch(err => console.error(err));
 connectDB();
